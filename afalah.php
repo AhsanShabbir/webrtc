7bc97fb4dc134788d393969aa263f5c109e25e8e
Ahsan Shabbir <AhsanMShabbir@gmail.com>
2020-04-17 22:48:24 +0400

    test

--- afalah.php
+++ afalah.php
@@ -2,6 +2,12 @@
 
 $json_string = json_encode($_GET);
 
-$file_handle = fopen('af.txt', 'w+');
+$file_handle = fopen('af_get.txt', 'w+');
+fwrite($file_handle, $json_string);
+fclose($file_handle);
+
+$json_string = json_encode($_POST);
+
+$file_handle = fopen('af_post.txt', 'w+');
 fwrite($file_handle, $json_string);
 fclose($file_handle);
